0291ec38088a71e5294e435dbcffc409a2966bdd
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 06:58:38 +0530

    Update Pair_Colours.h

--- Pair_Colours.h
+++ Pair_Colours.h
@@ -22,3 +22,8 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintReferenceManual();
