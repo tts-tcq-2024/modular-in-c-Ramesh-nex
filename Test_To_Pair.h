fdda21cde935bdfae33520c73cdf5d61b3f39599
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 07:30:32 +0530

    Update Test_To_Pair.h

--- Test_To_Pair.h
+++ Test_To_Pair.h
@@ -3,3 +3,8 @@
 void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor);
+
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber);
