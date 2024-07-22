ed07af46d933b5ecbbcfcf06a4826b0c01f5d8f5
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 07:24:18 +0530

    Update and rename Pair_Colours.h to Pair_Colors.h

--- Pair_Colors.h
+++ Pair_Colors.h
@@ -8,12 +8,20 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+// Added as extern definition as the definition will be in the c file 
 extern const char* MajorColorNames[];
 extern const char* MinorColorNames[];
 extern int numberOfMajorColors;
 extern int numberOfMinorColors;
 
+/* Interface to Color pair to string */
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
+
+/* Interface to Get color from pair number */
 ColorPair GetColorFromPairNumber(int pairNumber);
+
+/* Interface to Get Pair number from color */
 int GetPairNumberFromColor(const ColorPair* colorPair);
+
+/* Interface to Print reference Manual */
 void PrintReferenceManual();
