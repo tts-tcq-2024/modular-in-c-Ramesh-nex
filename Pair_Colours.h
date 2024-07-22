9299c6351841e6b2493b6707fcf0975ea272e1d7
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 07:14:03 +0530

    Update Pair_Colours.h

--- Pair_Colours.h
+++ Pair_Colours.h
@@ -3,8 +3,6 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
