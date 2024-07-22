e338404b405c01520b83228c185453e61bdae6f0
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 07:02:07 +0530

    Update Pair_Colours.c

--- Pair_Colours.c
+++ Pair_Colours.c
@@ -1,3 +1,14 @@
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
@@ -19,3 +30,11 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
             colorPair->minorColor + 1;
 }
 
+void PrintReferenceManual() {
+    char colorPairNames[16];
+    for (int pairNumber = 1; pairNumber <= numberOfMajorColors * numberOfMinorColors; pairNumber++) {
+        ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+        ColorPairToString(&colorPair, colorPairNames);
+        printf("%d: %s\n", pairNumber, colorPairNames);
+    }
+}
