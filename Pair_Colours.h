67738228bae66bd9d6ce55bc5cb019efdf7d8726
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 07:09:50 +0530

    Update Pair_Colours.h

--- Pair_Colours.h
+++ Pair_Colours.h
@@ -5,19 +5,13 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
 
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
 extern int numberOfMajorColors;
 extern int numberOfMinorColors;
 
